d672274bd1b70a9606d08bee513d2a54279d2c3f
yyk <yyk@example.com>
2019-07-31 17:27:21 +0800

    two

--- yyk-parent/eureka-client/src/main/java/com/yyk/ZullApplication.java
+++ yyk-parent/eureka-client/src/main/java/com/yyk/ZullApplication.java
@@ -1,9 +1,13 @@
 package com.yyk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +17,10 @@ public class ZullApplication {
 	        SpringApplication.run(ZullApplication.class, args);
 	    }
 
-	
+	    @Bean
+	    @Autowired
+	    @LoadBalanced  //@LoadBalanced 表示开启负载均衡
+	    RestTemplate restTemplate(){
+	        return new RestTemplate();
+	    }
 }
